1f204eb60402e857fde87e690c1b8244d5ae5835
verpenunes <106631829+verpenunes@users.noreply.github.com>
2023-10-03 00:16:38 +0200

    Message error

--- js/contact.js
+++ js/contact.js
@@ -76,7 +76,7 @@ textBox.addEventListener("keyup", checkIfButtonIsDisabled);
 function submitForm(event) {
     event.preventDefault();
 
-    message.innerHTML = `<div class="message">Your message has been sent.</div>`;
+    message.innerHTML = `<div class="message">Your message has been sent</div>`;
     form.reset();
 }
 
